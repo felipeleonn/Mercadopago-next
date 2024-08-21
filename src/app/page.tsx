54163660f7c3e050c0dea6b977c73a3a47b96a43
felipeleonn <felipeleon98@outlook.com>
2024-08-20 22:55:32 -0300

    Set up payment and supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from "@/components/ui/table";
 import { Textarea } from "@/components/ui/textarea";
+import { createClient } from "@supabase/supabase-js";
 import { MercadoPagoConfig, Preference } from 'mercadopago';
 import { redirect } from "next/navigation";
-const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN! });
 
-export default function HomePage() {
+const mercadopago = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN! });
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SECRET!)
+
+export default async function HomePage() {
+  const donations = await supabase
+    .from("donaciones")
+    .select("*")
+    .then(
+      ({data}) =>
+        data as unknown as Promise<
+          {id: number; created_at: number; amount: number; message: string}[]
+        >,
+    );
   async function donate(formData: FormData) {
     'use server'
 
-    const preference = await new Preference(client).create({
+    const preference = await new Preference(mercadopago).create({
       body: {
         items: [
           {
@@ -23,21 +36,58 @@ export default function HomePage() {
       },
     });
 
-      // te trae una url a la cual mandar al usuario
-      redirect(preference.sandbox_init_point!)
+    // console.log(preference)
+    // te trae una url a la cual mandar al usuario en dev
+    // redirect(preference.sandbox_init_point!)
+
+    // Usar init_point en produccion
+    redirect(preference.init_point!)
   }
   return (
-    <form action={donate} className="m-auto grid max-w-96 gap-6 border p-4">
-      <Label className="grid gap-2">
-        <span>Valor</span>
-        <Input type="number" name="amount"/>
-      </Label>
-      <Label className="grid gap-2">
-        <span>Tu mensaje en la donacion</span>
-        <Textarea name="message"/>
-      </Label>
-      <Button type="submit">Enviar</Button>
-    </form>
+    <section className="m-auto grid min-h-screen grid-rows-[auto,1fr,auto] bg-background px-4 font-sans antialiased">
+      <main className="py-8">
+        <header className="text-center text-xl font-bold leading-[4rem]">Integracion MercadoPago</header>
+        <section className="flex flex-col items-center gap-12">
+          <form action={donate} className="grid w-full max-w-md gap-6 border p-4">
+            <Label className="grid gap-2">
+              <span>Valor</span>
+              <Input type="number" name="amount" />
+            </Label>
+            <Label className="grid gap-2">
+              <span>Tu mensaje en la donacion</span>
+              <Textarea name="message" />
+            </Label>
+            <Button type="submit">Enviar</Button>
+          </form>
+          <Table className="w-full" >
+            <TableHeader>
+              <TableRow>
+                <TableHead>Cantidad</TableHead>
+                <TableHead className="text-right">Mensaje</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {donations.map((donation) => {
+                return (
+                  <TableRow key={donation.id}>
+                    <TableCell className="font-bold">
+                      {donation.amount.toLocaleString("es-AR", {
+                        style: "currency",
+                        currency: "ARS",
+                      })}
+                    </TableCell>
+                    <TableCell className="text-right">{donation.message}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </section>
+      </main>
+      <footer className="text-center leading-[4rem] opacity-70">
+        © {new Date().getFullYear()} Felipe Leon
+      </footer>
+    </section>
   );
 
 }
--- src/app/payment/route.ts
+++ src/app/payment/route.ts
@@ -1,7 +1,36 @@
+import { MercadoPagoConfig , Payment } from "mercadopago";
 import { NextRequest } from "next/server";
+import { createClient } from "@supabase/supabase-js";
+
+const mercadopago = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN! });
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SECRET!)
 
 export async function POST(request: NextRequest) {
-    const body = await request.json()
-    console.log("body:", body)
+    const body = await request.json().then((data) => data as {data: {id: string}})
+    const payment = await new Payment(mercadopago).get({id: body.data.id})
+    console.log("payment:", payment)
+
+    const donation = {
+        id: payment.id,
+        amount: payment.transaction_amount,
+        message: payment.description,
+    }
+
+    await supabase.from("donaciones").insert(donation)
+
     return Response.json({success: true})
 }
+
+// export async function GET() {
+
+//     const donation = {
+//         id: 1,
+//         amount: 1000,
+//         message: "test",
+//     }
+
+//     const result = await supabase.from("donaciones").insert(donation)
+//     console.log(result)
+
+//     return Response.json({success: true})
+// }
